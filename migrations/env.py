c90633cdeb4f9f577e3f321aede5d7655fd9111d
Ryan O. <ryanosterday@mac.com>
2026-07-12 07:35:00 -0400

    Alembic autogenerates 00n file names

--- migrations/env.py
+++ migrations/env.py
@@ -2,6 +2,7 @@ import asyncio
 from logging.config import fileConfig
 
 from alembic import context
+from alembic.script import ScriptDirectory
 from sqlalchemy.ext.asyncio import create_async_engine
 
 from app.config import settings
@@ -12,6 +13,22 @@ config = context.config
 fileConfig(config.config_file_name)  # type: ignore[arg-type]
 
 target_metadata = ModelBase.metadata
+
+
+def process_revision_directives(_context, _revision, directives):
+    """Zero-padded sequential revision ids (001, 002, 003…) instead of GUID hashes."""
+    if not directives:
+        return
+    scriptDirectory: ScriptDirectory = ScriptDirectory.from_config(config)
+    max_rev_num: int = 0
+    for rev in scriptDirectory.walk_revisions():
+        if not rev.revision.isdigit():
+            continue
+        max_rev_num = max(max_rev_num, int(rev.revision))
+
+    max_rev_num += 1  # We want the next one, lol
+    max_rev_str: str = f"{max_rev_num:03d}"
+    directives[0].rev_id = max_rev_str
 
 
 def run_migrations_offline() -> None:
@@ -31,7 +48,9 @@ async def run_migrations_online() -> None:
     async with connectable.connect() as connection:
         await connection.run_sync(
             lambda sync_conn: context.configure(
-                connection=sync_conn, target_metadata=target_metadata
+                connection=sync_conn,
+                target_metadata=target_metadata,
+                process_revision_directives=process_revision_directives,
             )
         )
         async with connection.begin():
